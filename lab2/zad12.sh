b2bd16d1849728862890ccbf33e8307555a52956
Werdon4 <filipgajewski4@gmail.com>
2020-03-03 18:13:57 +0100

    Mastered file with shellcheck

--- lab2/zad12.sh
+++ lab2/zad12.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-files=`find $1/* -maxdepth 0 -perm -u=w -type f | wc -l`
-directories=`find $1/* -maxdepth 0 -perm -u=w -type d | wc -l`
+files=$(find "$1"/* -maxdepth 0 -perm -u=w -type f | wc -l)
+directories=$(find "$1"/* -maxdepth 0 -perm -u=w -type d | wc -l)
 
-echo "Plikow: "$files
-echo "Katalogow: "$directories
+echo "Plikow: ""$files"
+echo "Katalogow: ""$directories"
