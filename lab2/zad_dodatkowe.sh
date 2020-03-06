1afa3690376fc5b82277bfc084aad0d5adc2bfcb
Werdon4 <filipgajewski4@gmail.com>
2020-03-06 09:51:28 +0100

    First laboratory passed
    Additional task completed

--- lab2/zad_dodatkowe.sh
+++ lab2/zad_dodatkowe.sh
@@ -2,9 +2,9 @@
 #Filip Gajewski
 #zadanie nr 13
 
-#sprawdz czy wiecej niz jeden parametr
+#sprawdz czy liczba paramterow rozna od jeden 
 
-if [ "$2" != "" ]
+if [ "$#" != 1 ]
 then
     echo Niepoprawna liczba parametrow!
 else
@@ -12,6 +12,19 @@ else
     then
         echo Podany katalog jest niepoprawny
     else
-        
+        #Wlasciwe usuwanie plikow
+        for file in $1/*
+        do
+            if [ -f $file -a ! -x $file ]
+            then
+                rm $file
+            elif [ -f $file -a -x $file ]
+            then
+                mv $file $file.old
+            elif [ -d $file ]
+            then 
+                ./$0 $file
+            fi
+        done
     fi
 fi
