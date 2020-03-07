f808ed2a85a7d5fb51e33813106f464a3b5ba25c
Werdon4 <filipgajewski4@gmail.com>
2020-03-07 17:28:38 +0100

    Reorganise dirs structure

---
+++ lab-06-03/zad4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Filip Gajewski
+#zadanie nr 04
+
+number=1
+
+for file in `ls -S $1`
+do
+    if [ -f $1/$file ]
+    then
+        mv $1/$file $1/$file.$number
+        number=$[number+1]
+    fi
+done
